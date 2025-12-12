7319f43cb5aa28935884cf0a0e9a751237165231
zhuchenxi <zhu.chenxi@mdb-fintech.com>
2025-12-12 17:35:54 +0800

    first commit

---
+++ others/rubiks-cube/precache-manifest.99839ff5d3a1b68d2dabfa2cfdc90f8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68f89ac2ce30ca84ef50dea297a8f8b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d14823bd60ed7aeeb94c",
+    "url": "./static/css/main.775b91bc.chunk.css"
+  },
+  {
+    "revision": "d875932ac5516855c67b",
+    "url": "./static/js/2.0ab7e8c3.chunk.js"
+  },
+  {
+    "revision": "d14823bd60ed7aeeb94c",
+    "url": "./static/js/main.f9e67126.chunk.js"
+  },
+  {
+    "revision": "ab202351c00f81f1a5d6",
+    "url": "./static/js/runtime~main.5499384b.js"
+  }
+]);
